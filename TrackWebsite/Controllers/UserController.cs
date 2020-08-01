68aa5972942cdb95c9c696fefa59ad29a53766f6
MMokoena <MokoenaM720@gmail.com>
2020-08-01 20:10:10 +0200

    initial upload

---
+++ TrackWebsite/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrackWebsite.Controllers
+{
+    public class UserController : Controller
+    {
+       //Registration Action
+       [HttpGet]
+       public ActionResult Registration()
+        {
+            return View(); 
+        }
+        //Registration post action 
+
+
+        //Verify Email
+
+
+        //Verify Email Link 
+
+
+
+
+
+        //Login action 
+
+
+        //Login post action
+
+
+        //Logout
+    }
+}
